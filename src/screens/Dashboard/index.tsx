31fc4e095b9c9487f356ca89df38b8a76c50861a
OtavioFilipe <darksnack16@gmail.com>
2022-04-28 23:03:21 -0300

    :rocket: add and applying measure proportions and adding avatar from github

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 
-import { Container, Title } from "./style";
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from "./style";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: 'https://avatars.githubusercontent.com/u/75401844?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Otávio</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
-  );
+  )
 }
