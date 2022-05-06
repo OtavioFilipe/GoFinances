57aceef26ad39519d04147df9e34c445bea8a5e4
OtavioFilipe <darksnack16@gmail.com>
2022-05-05 21:04:32 -0300

    :rocket: leaving dynamic transaction cards

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -17,7 +17,7 @@ interface Category {
 }
 
 interface Data {
-
+    type: "positive" | "negative";
     title: string;
     amount: string;
     category: Category;
@@ -35,12 +35,13 @@ export function TransactionCard({ data }: Props) {
             <Title>
                 {data.title}
             </Title>
-            <Amount>
+            <Amount type={data.type}>
+                {data.type === "negative" && "- "}
                 {data.amount}
             </Amount>
             <Footer>
                 <Category>
-                    <Icon name="dollar-sign" />
+                    <Icon name={data.category.icon} />
                     <CategoryName>
                         {data.category.name}
                     </CategoryName>
