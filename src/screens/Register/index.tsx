b840a4eb0d24239acc7b5549b575fffa0767c5c8
OtavioFilipe <darksnack16@gmail.com>
2022-05-07 19:39:36 -0300

    :rocket:  create button select category

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { Button } from "../../components/Form/Button";
 import { Input } from "../../components/Form/Input"
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelect } from "../../components/Form/CategorySelect";
+
 
 
 import { 
@@ -51,6 +53,8 @@ export function Register() {
 
                     />
                 </TransactionsTypes>
+
+                <CategorySelect title="Categoria"/>
             </Fields>
         
             <Button title="Enviar"/>
