4fef210ab6d8692026d0d73f782230d49ba1e159
OtavioFilipe <darksnack16@gmail.com>
2022-06-15 22:43:10 -0300

    :rocket: add async storage and listing function

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { 
     Modal, 
     TouchableWithoutFeedback, 
@@ -8,8 +8,10 @@ import {
 import * as Yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup"
 import AsyncStorage from "@react-native-async-storage/async-storage"
+import uuid from "react-native-uuid"
 
 import { useForm } from "react-hook-form";
+import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../../components/Form/Button";
 import { InputForm } from "../../components/Form/InputForm";
@@ -55,9 +57,12 @@ export function Register() {
     name: "Categoria",
   });
 
+  const navigation = useNavigation();
+
   const { 
       control, 
       handleSubmit,
+      reset,
       formState: { errors }
     } = useForm({
         resolver: yupResolver(schema)
@@ -82,30 +87,40 @@ export function Register() {
     if(category.key === "category")
     return Alert.alert("Selecione a categoria");
     
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
       category: category.key,
+      date: new Date()
     };
 
     try {
-      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+      
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      navigation.navigate("Listagem");
 
     } catch (error) {
       console.log(error);
       Alert.alert("Não foi possível salvar");
     }
   }
-
-  useEffect(() => {
-    async function loadData(){
-      const data = await AsyncStorage.getItem(dataKey);
-      console.log(JSON.parse(data!));
-    }
-
-    loadData();
-  },[])
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
