a9ccbdb53a6a4bbe1ee93d8bcf2b2354c66444e6
OtavioFilipe <darksnack16@gmail.com>
2022-08-11 22:22:05 -0300

    create signin page

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { AppRoutes } from "./src/routes/app.routes";
+
+import { SignIn } from "./src/screens/SignIn";
 
 import {
   Poppins_400Regular,
@@ -32,7 +33,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
